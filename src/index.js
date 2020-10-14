3310a5ab27721b7da655956addc621346dd6c4d9
yaroslavklimuk21 <yaroslavklimuk21@gmail.com>
2020-10-14 20:26:07 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+    let symbols = str.split("");
+    let openBrackets = ["{", "(", "["];
+    let closeBrackets = ["}", ")", "]"];
+    let closeIndex;
+    let openIndex;
+
+    for (let i = 0; i < symbols.length; i++) {
+        openIndex = openBrackets.indexOf(symbols[i]);
+        if (openIndex !== -1) {
+            stack.push(openIndex);
+            continue;
+        }
+        closeIndex = closeBrackets.indexOf(symbols[i]);
+        if (closeIndex !== -1) {
+            openIndex = stack.pop();
+            if (closeIndex !== openIndex) {
+                return false;
+            }
+        }
+    }
+    if (stack.length !== 0) {
+        return false;
+    }
+    return true;
+};
